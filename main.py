73ebb66e755c30cd14cc6e7ce3671c1522b3f9bb
Jakub Górski <100696401+jagor02@users.noreply.github.com>
2025-06-04 18:54:35 +0200

    dataset for CNN and some changes in main.py

--- main.py
+++ main.py
@@ -2,62 +2,66 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-# coords for perspective change
-coord = []
+# global variables
+persp_change_coords = []
 
 def click_event(event, x, y, flags, params):
     if event == cv2.EVENT_LBUTTONDOWN:
-        coord.append((x, y)) # the first = left down corner, the second - right down corner, the third - right up corner, the fourth - left up corner
+        persp_change_coords.append((x, y)) 
 
 if __name__ == "__main__":
+    # constants
+    tile_columns_names = 'abcdefgh'
+    tile_rows_names = '12345678'
+    lines_intersection_pattern = np.zeros((3, 3, 3), dtype=np.uint8)
+    lines_intersection_pattern[:, :, 2] = np.array([[0, 255, 0], [255, 255, 255], [0, 255, 0]], dtype=np.uint8)
+    intersection_dist = 10
+    first_iter = True
+
+    # variables
+    tiles_found = False
+    intersection_coords = np.zeros(shape=(9, 9), dtype=tuple)
+
     # camera options
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 800)
-    cv2.namedWindow("Coords for perspective change")
-    cv2.setMouseCallback("Coords for perspective change", click_event)
 
-    # getting coord for the perspetive change
-    while len(coord) < 4:
-        _, frame = cap.read() # real-time
-        #frame = cv2.imread("photos/chessboard.jpg") # simulation
+    # getting coords for the perspetive change
+    cv2.namedWindow("Select chessboard")
+    cv2.setMouseCallback("Select chessboard", click_event)
 
-        # viewing
-        cv2.imshow("Coords for perspective change", frame)
+    while len(persp_change_coords) < 4:
+        # frame
+        _, frame = cap.read()
+        cv2.imshow("Select chessboard", frame) # the first coords - the top left corner, the second coords - the top right corner, the third coords - the right bottom corner, the fourth coords - left bottom corner
         cv2.waitKey(1)
 
-    cv2.destroyAllWindows()
+    cv2.destroyWindow("Select chessboard")
 
-    tile_side_long_computed_flag = False
+    # naming the chessboard tiles
+    tiles_names = [f"{col}{row}" for row in tile_rows_names for col in tile_columns_names]
 
-    columns = 'abcdefgh'
-    rows = '12345678'
+    # the perspective change matrix M
+    width_top_view_first_side = int(np.sqrt(((persp_change_coords[1][0]-persp_change_coords[0][0])**2)+((persp_change_coords[1][1]-persp_change_coords[0][1])**2)))
+    width_top_view_second_side = int(np.sqrt(((persp_change_coords[2][0]-persp_change_coords[3][0])**2)+((persp_change_coords[2][1]-persp_change_coords[3][1])**2)))
+    width_top_view_max = max(width_top_view_first_side, width_top_view_second_side)
+    
+    height_top_view_first_side = int(np.sqrt(((persp_change_coords[0][0]-persp_change_coords[3][0])**2)+((persp_change_coords[0][1]-persp_change_coords[3][1])**2)))
+    height_top_view_second_side = int(np.sqrt(((persp_change_coords[1][0]-persp_change_coords[2][0])**2)+((persp_change_coords[1][1]-persp_change_coords[2][1])**2)))
+    height_top_view_max = max(height_top_view_first_side, height_top_view_second_side)
+    
+    persp_change_taken_coords = np.float32(persp_change_coords)
+    persp_change_coords_top_view = np.float32([[0, 0], [width_top_view_max-1, 0], [width_top_view_max-1, height_top_view_max-1], [0, height_top_view_max-1]])
 
-    tiles_names = [f"{col}{row}" for row in rows for col in columns]
-
-    intersection_coord = np.zeros(shape=(9, 9), dtype=tuple)
+    M = cv2.getPerspectiveTransform(persp_change_taken_coords, persp_change_coords_top_view)
 
     while True:
         # frame
-        _, frame = cap.read() # real-time
-        #frame = cv2.imread("photos/chessboard.jpg") # simulation
+        _, frame = cap.read()
 
-        width_top_view_1 = np.sqrt(((coord[1][0] - coord[0][0])**2) + ((coord[1][1] - coord[0][1])**2))
-        width_top_view_2 = np.sqrt(((coord[2][0] - coord[3][0])**2) + ((coord[2][1] - coord[3][1])**2))
-        width_top_view = max(int(width_top_view_1), int(width_top_view_2))
-        
-        height_top_view_1 = np.sqrt(((coord[3][0] - coord[0][0])**2) + ((coord[3][1] - coord[0][1])**2))
-        height_top_view_2 = np.sqrt(((coord[2][0] - coord[1][0])**2) + ((coord[2][1] - coord[1][1])**2))
-        height_top_view = max(int(height_top_view_1), int(height_top_view_2))
-        
-        coord_from_image = np.float32(coord)
-        coord_top_view = np.float32([[0, 0], [width_top_view-1, 0], [width_top_view-1, height_top_view-1], [0, height_top_view-1]])
-
-        M = cv2.getPerspectiveTransform(coord_from_image, coord_top_view)
-
-        top_view = cv2.warpPerspective(frame, M,(width_top_view, height_top_view), flags=cv2.INTER_LINEAR)
-
-        height, width, _ = np.shape(top_view)
+        # perspective change
+        top_view = cv2.warpPerspective(frame, M, (width_top_view_max, height_top_view_max), flags=cv2.INTER_LINEAR)
 
         # light equalization - CLAHE algorithm
         lab = cv2.cvtColor(top_view, cv2.COLOR_BGR2LAB)
@@ -70,84 +74,82 @@ if __name__ == "__main__":
         # smoothing
         gaussian = cv2.GaussianBlur(clahe_top_view, (7, 7), 3)
 
-        # finding the chessboard
-        hsv = cv2.cvtColor(gaussian, cv2.COLOR_BGR2HSV)
-        gray = cv2.cvtColor(gaussian, cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(gray, 60, 150)
-        lines = cv2.HoughLines(edges, 1, np.pi/180, 50)
-        r_table = []
-        theta_table = []
-        lines_view = np.zeros(np.shape(top_view), dtype=np.uint8)
-        if lines is not None:
-            for r_and_theta in lines:
-                r, theta = r_and_theta[0]
-                r_table.append(r)
-                theta_table.append(theta)
-                if np.sin(theta) == 0:
-                    x1, x2 = int(r), int(r)
-                    y1, y2 = 0, height-1
-                else:
-                    x1, x2 = 0, width-1
-                    a = -np.cos(theta)/np.sin(theta)
-                    b = r/np.sin(theta)
-                    y1 = int(a*x1+b)
-                    y2 = int(a*x2+b)
-                # limiting lines
-                y1 = max(0, min(height-1, y1))
-                y2 = max(0, min(height-1, y2))
-                if np.isclose(theta, np.pi/2, atol=0.01) or np.isclose(theta, 0, atol=0.01) or np.isclose(theta, np.pi, atol=0.01):
+        # finding tiles
+        if not tiles_found:
+            gray = cv2.cvtColor(gaussian, cv2.COLOR_BGR2GRAY)
+            edges = cv2.Canny(gray, 60, 150)
+            lines = cv2.HoughLines(edges, 1, np.pi/180, 50)
+            lines_view = np.zeros(np.shape(top_view), dtype=np.uint8)
+            if lines is not None:
+                for r_and_theta in lines:
+                    r, theta = r_and_theta[0]
+                    if np.isclose(theta, 0, atol=0.01) or np.isclose(theta, np.pi, atol=0.01):
+                        x1, x2 = int(r), int(r)
+                        y1, y2 = 0, height_top_view_max-1
+                    elif np.isclose(theta, np.pi/2, atol=0.01):
+                        x1, x2 = 0, width_top_view_max-1
+                        a = -np.cos(theta)/np.sin(theta)
+                        b = r/np.sin(theta)
+                        y1 = int(a*x1+b)
+                        y2 = int(a*x2+b)
+                    else:
+                        continue
+                    # limiting lines
+                    y1 = max(0, min(height_top_view_max-1, y1))
+                    y2 = max(0, min(height_top_view_max-1, y2))
                     cv2.line(lines_view, (x1, y1), (x2, y2), (0, 0, 255), 1)
-        lines_intersection = np.zeros((3, 3, 3), dtype=np.uint8)
-        lines_intersection[:, :, 2] = np.array([[0, 255, 0], [255, 255, 255], [0, 255, 0]], dtype=np.uint8)
-        intersection_view = np.zeros(np.shape(top_view), dtype=np.uint8)
-        if not tile_side_long_computed_flag:
-            row = 0
-            col = 0
-            for i in range(1, height-1):
-                for j in range(1, width-1):
-                    window = lines_view[max(i-1, 0):min(i+2, height), max(j-1, 0):min(j+2, width), :]
-                    if window.shape == (3, 3, 3) and np.all(window == lines_intersection):
-                        if np.any(intersection_view[max(i-10, 0):min(i+11, height), max(j-10, 0):min(j+11, width), 2] == 255):
-                            continue
-                        else:
-                            intersection_view[i, j, 2] = 255
-                            intersection_coord[row, col] = (i, j)
-                            col += 1
-                            if col > 8:
-                                row += 1
-                                col = 0
-                            if row > 8:
-                                break
-                if row > 8:
-                    break
-            if np.sum(intersection_view[:, :, 2]) != 9*9*255:
-                continue
-            #tile_diameter = np.sqrt(((intersection_coord[8, 8])[0]-(intersection_coord[0, 0])[0])**2 + ((intersection_coord[8, 8])[1]-(intersection_coord[0, 0])[1])**2)/8
-            #tile_side_long = int(tile_diameter/np.sqrt(2))
-            tile_side_long_computed_flag = True     
+                intersection_view = np.zeros(np.shape(top_view), dtype=np.uint8)
+                intersection_row = 0
+                intersection_col = 0
+                for i in range(1, height_top_view_max-1):
+                    for j in range(1, width_top_view_max-1):
+                        window = lines_view[i-1:i+2, j-1:j+2, :]
+                        if np.all(window == lines_intersection_pattern):
+                            if np.any(intersection_view[max(i-intersection_dist, 0):min(i+intersection_dist+1, height_top_view_max), max(j-intersection_dist, 0):min(j+intersection_dist+1, width_top_view_max), 2] == 255):
+                                continue
+                            else:
+                                intersection_view[i, j, 2] = 255
+                                intersection_coords[intersection_row, intersection_col] = (i, j)
+                                intersection_col += 1
+                                if intersection_col > 8:
+                                    intersection_row += 1
+                                    intersection_col = 0
+                                if intersection_row > 8:
+                                    break
+                    if intersection_row > 8:
+                        break
+                if np.sum(intersection_view[:, :, 2]) != 9*9*255:
+                    continue
+                tiles_found = True
 
+        # the dictionary of the tiles view
         tiles_view = []
 
         for i in range(8):
             for j in range(8):
-                y_first, x_first = intersection_coord[i, j]
-                _, x_last= intersection_coord[i, j+1]
-                y_last, _ = intersection_coord[i+1, j]
-                tile = np.array(top_view[y_first:y_last, x_first:x_last, :], dtype=np.uint8)
-                #tile = np.array(top_view[coord[0]:coord[0]+tile_side_long, coord[1]:coord[1]+tile_side_long, :], dtype=np.uint8)
-                tile = cv2.resize(tile, None, fx=10.0, fy=10.0, interpolation=cv2.INTER_LINEAR)
+                y_first, x_first = intersection_coords[8-i-1, j]
+                _, x_last= intersection_coords[8-i-1, j+1]
+                y_last, _ = intersection_coords[8-i, j]
+                tile = np.array(top_view[y_first:y_last+1, x_first:x_last, :], dtype=np.uint8)
                 tiles_view.append(tile)
 
         tiles = {names:views for (names, views) in zip(tiles_names, tiles_view)} 
 
         # viewing
-        cv2.imshow("View", tiles["h3"])
+        if first_iter:
+            keys = iter(tiles)
+            tile_name = next(keys)
+            first_iter = False
+        cv2.imshow("View", tiles[tile_name])
         key = cv2.waitKey(1)  # wait for the key ('Esc')
         if key == 27:
             break
-
-    plt.scatter(theta_table, r_table)
-    plt.show()
+        elif key == 97: # 'a'
+            cv2.imwrite("./dataset/test/black_knight/black_knight_"+tile_name+".jpg", tiles[tile_name])
+            if tile_name == "h8":
+                break
+            else:
+                tile_name = next(keys)
 
     # cleaning
     cap.release()
